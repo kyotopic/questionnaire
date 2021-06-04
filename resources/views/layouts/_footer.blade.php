731078a91c453f2753fec9939941f25e55db2441
kyotopic <rentongkyo@gmail.com>
2021-06-04 07:16:51 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      该问卷由 <a href="http://" target="_blank">xx</a> 所有 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+  </div>
+</footer>
