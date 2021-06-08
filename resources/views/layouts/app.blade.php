092ee94e70c5cab90a8ab2b87211989fd210fefd
kyotopic <rentongkyo@gmail.com>
2021-06-08 06:03:27 +0000

    静态页面设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,38 +2,87 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'Questionnaire') - Questionnaire 传统艺人商品购买意向问卷</title>
+    <title>@yield('title', 'Questionnaire') - Questionnaire 传统艺人商品购买意向问卷</title>
 
-  <!-- Styles -->
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
-  <div id="app" class="{{ route_class() }}-page">
+    <div id="app" class="{{ route_class() }}-page">
 
-    @include('layouts._header')
+        @include('layouts._header')
 
-    <div class="container">
+        <div class="container">
 
-      @include('shared._messages')
+            @include('shared._messages')
 
-      @yield('content')
+            @yield('content')
 
+        </div>
+
+        @include('layouts._footer')
     </div>
 
-    @include('layouts._footer')
-  </div>
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script>
+    $(document).ready(function() {
+        var id;
+        $("#imagescheckcups a").bind("click", function() {
+            id = $(this).children("input").attr("id");
+        });
+        $("#imagescheckteapots a").bind("click", function() {
+            id = $(this).children("input").attr("id");
+        });
+        $("#imagescheckceramics a").bind("click", function() {
+            id = $(this).children("input").attr("id");
+        });
+        $("#imageschecksilver a").bind("click", function() {
+            id = $(this).children("input").attr("id");
+        });
+        $("#imagescheckweave a").bind("click", function() {
+            id = $(this).children("input").attr("id");
+        });
+        $("#imagescheckpaint a").bind("click", function() {
+            id = $(this).children("input").attr("id");
+        });
 
-  <!-- Scripts -->
-  <script src="{{ mix('js/app.js') }}"></script>
+        $("#save1").click(function() {
+            $("#price3001200").modal('hide');
+            $("#" + id + "").attr("checked", true);
+        });
+        $("#close1").click(function() {
+            $("#price3001200").modal('hide');
+            $("#" + id + "").attr("checked", false);
+        });
+        $("#save2").click(function() {
+            $("#price6002400").modal('hide');
+            $("#" + id + "").attr("checked", true);
+        });
+        $("#close2").click(function() {
+            $("#price6002400").modal('hide');
+            $("#" + id + "").attr("checked", false);
+        });
+        $("#save3").click(function() {
+            $("#price500030000").modal('hide');
+            $("#" + id + "").attr("checked", true);
+        });
+        $("#close3").click(function() {
+            $("#price500030000").modal('hide');
+            $("#" + id + "").attr("checked", false);
+        });
+
+    });
+    </script>
 </body>
 
 </html>
